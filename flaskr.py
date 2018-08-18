b55b1cbb40901d9d6669b50122c4a448d3958d87
Ilya <s3530184@student.rmit.edu.au>
2018-08-18 14:25:51 +1000

    flask integration with DB

---
+++ flaskr.py
@@ -0,0 +1,14 @@
+import sqlite3
+app.database = "sensehat_log.db"
+
+@app.route('/weatherlogger')
+def weatherdata():
+    g.db = connect.db()
+    cur = g.db.execute('SELECT * from data_entry')
+    Data = [dict(ID=row[0],
+                    Temperature=column[1],
+                    Humidity=column[2],
+                    Pressure=column[3],
+                    Time=column[4]) for column in cur.fetchall()]
+    g.db.close()
+    return render_template('index.html', Data=Data)
